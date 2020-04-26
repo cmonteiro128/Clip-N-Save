fd1a361145faba2bfc3aa690ebc6617e5816f2aa
Chris Monteiro <smilezxd13@gmail.com>
2020-04-26 16:57:58 -0400

    test api url

--- client/src/environments.js
+++ client/src/environments.js
@@ -1,5 +1,5 @@
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.API_URL // add --env.API_URL=https://clipnsave.now.sh/ when using build
+    ? process.env.API_URL // add --env.API_URL=https://example.com/ when using build locally
     : 'http://localhost:1337/';
 export default baseURL;
