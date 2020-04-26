fdd2968b19bf138f204b96b58fd551cd59ee022c
Chris Monteiro <smilezxd13@gmail.com>
2020-04-26 17:00:58 -0400

    Placeholder API URL

--- client/src/environments.js
+++ client/src/environments.js
@@ -1,5 +1,5 @@
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.API_URL // add --env.API_URL=https://example.com/ when using build locally
+    ? 'https://example.com'
     : 'http://localhost:1337/';
 export default baseURL;
